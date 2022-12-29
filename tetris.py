ef519f46fd373b52eab84270b75647b99a721316
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-12-29 23:02:33 +0100

    siguiendo

--- tetris.py
+++ tetris.py
@@ -35,12 +35,60 @@ class Tetris(QMainWindow):
         self.center()
         self.setWindowTitle('Tetris')
         self.show()
-    #funciín para que la ventana esté centrada en la pantalla
+    #función para que la ventana esté centrada en la pantalla
     def center(self):
         """centers the window on the screen"""
 
         screen = QDesktopWidget().screenGeometry()
         size = self.geometry()
         self.move(int((screen.width() - size.width()) / 2),
-                  int((screen.height() - size.height()) / 2))
+                    int((screen.height() - size.height()) / 2))
+
+
+#esta clase será la encargada de incicializar el juego
+class Board(QFrame):
+    msg2Statusbar = pyqtSignal(str)
+
+    # se establecen parámetros básicos (ancho, alto y velocidad de figuras)
+    BoardWidth = 10
+    BoardHeight = 22
+    Speed = 300
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.initBoard()
+
+    #función de inicio
+    def initBoard(self):
+        #se inicia el tiempo y va bajando la figura
+        self.timer = QBasicTimer()
+        self.isWaitingAfterLine = False
+
+        self.curX = 0
+        self.curY = 0
+        self.numLinesRemoved = 0
+        self.board = []
+
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.isStarted = False
+        self.isPaused = False
+        self.clearBoard()
+
+
+    #altura de la tabla
+    def shapeAt(self, x, y):
+        return self.board[(y * Board.BoardWidth) + x]
+
+    #la froma de la tabla
+    def setShapeAt(self, x, y, shape):
+        self.board[(y * Board.BoardWidth) + x] = shape
+
+    #ancho de la tabla
+    def squareWidth(self):
+        return self.contentsRect().width() // Board.BoardWidth
+
+    #la altura de un cuadrado
+    def squareHeight(self):
+        return self.contentsRect().height() // Board.BoardHeight
 
